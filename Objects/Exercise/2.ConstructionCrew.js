8f561948a853e7498e2debc7d0beda4653bb61b6
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-26 20:50:37 +0300

    constructionCriew

--- Objects/Exercise/2.ConstructionCrew.js
+++ Objects/Exercise/2.ConstructionCrew.js
@@ -3,8 +3,9 @@ function constructionCrew(worker) {
         worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
         worker.dizziness == false;
     }
+    return worker;
 }
 
-constructionCrew({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true });
-constructionCrew({ weight: 120, experience: 20, levelOfHydrated: 200, dizziness: true });
-constructionCrew({ weight: 95, experience: 3, levelOfHydrated: 0, dizziness: false });
+console.log(constructionCrew({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true }));
+console.log(constructionCrew({ weight: 120, experience: 20, levelOfHydrated: 200, dizziness: true }));
+console.log(constructionCrew({ weight: 95, experience: 3, levelOfHydrated: 0, dizziness: false }));
