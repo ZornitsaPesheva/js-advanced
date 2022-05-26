6aab335443e7d6a89d4091a7662b78d5f83b5557
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-05-26 17:34:11 +0300

    worker

--- Objects/Exercise/2.ConstructionCrew.js
+++ Objects/Exercise/2.ConstructionCrew.js
@@ -1,11 +1,7 @@
-function constructionCrew() {
-
+function constructionCrew(worker) {
+    
 }
 
 constructionCrew({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true });
-constructionCrew({ weight: 120, experience: 20, 
-    
-    levelOfHydrated: 200,
-    
-    dizziness: true });
-constructionCrew();
+constructionCrew({ weight: 120, experience: 20, levelOfHydrated: 200, dizziness: true });
+constructionCrew({ weight: 95, experience: 3, levelOfHydrated: 0, dizziness: false });
