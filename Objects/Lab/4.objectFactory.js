2e77234ac1fc2f224813713edd4c06244a3ae753
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-26 06:14:53 +0300

    4

--- Objects/Lab/4.objectFactory.js
+++ Objects/Lab/4.objectFactory.js
@@ -1,15 +1,46 @@
-function cityRecord(name, population, treasury) {
-    const result = {
-       name,
-       population,
-       treasury
-    };
-    return result;
+function objectFactory(lib, list) {
+    return list.map(order => {
+        const object = Object.assign({}, order.template);
+
+        for (let part of order.parts) {
+            object[part] = lib[part];
+        }
+        return object;
+    })
 }
 
-console.log(cityRecord('Tortuga', 7000, 15000));
-console.log(cityRecord('Santo Domingo', 12000, 23500));
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
 
-const myCity = cityRecord('Tortuga', 7000, 15000);
-const {name, treasury} = myCity;
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
 
+const products = objectFactory(library, orders);
+console.log(products);
+
+//products[3].play('System Of A Down', 'Arials');
