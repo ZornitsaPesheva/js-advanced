8ccd1d9a6fda2b86bcd5f19964571061efeed76e
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-27 08:53:27 +0300

    car factory

--- Objects/Exercise/3.CarFactory.js
+++ Objects/Exercise/3.CarFactory.js
@@ -1,33 +1,14 @@
-function carFactory(car) {
-    setengine = function(car) { 
-        if (car.power >= 200) {
-            return car.engine = { power: 200, volume: 3500 }
-        }
-        else if (car.power >= 120) {
-            return car.engine = { power: 120, volume: 2400 }
-        } 
-        else { 
-            return car.engine = { power: 90, volume: 1800 }r
-        }
-    }
+function carFactory(wantedCar) {
+    let engines = [{power: 90, volume: 1800}, {power: 120, volume: 2400}, {power: 200, volume: 3500}];
+    let carriages = [{type:'hatchback', color: wantedCar.color}, {type: 'coupe', color: wantedCar.color}];
+    let wheelsize = wantedCar.wheelsize % 2 == 1 ? wantedCar.wheelsize : wantedCar.wheelsize - 1;
 
-    setcarriage = function(car) {
-        if (car.carriage == 'hatchback') {
-            return car.carriage = { tipe: 'hatchback' ,color: car.color }
-        }
-        else {
-            return car.carriage = { tipe: 'coupe' ,color: car.color }
-        }
-    }
-    
-    setWheels = function(car) {
-        
-    }
-
-    setengine(car);
-    setcarriage(car);
-
-    return car;
+    return {
+        model: wantedCar.model,
+        engine: engines.filter( e => e.power >= wantedCar.power )[0],
+        carriage: carriages.filter( c => c.type == wantedCar.carriage )[0],
+        wheels: [wheelsize, wheelsize, wheelsize, wheelsize]
+    };
 }
 
 console.log(carFactory({ model: 'VW Golf II', power: 90, color: 'blue', carriage: 'hatchback', wheelsize: 14 }));
