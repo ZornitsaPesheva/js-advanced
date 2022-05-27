cc8f41d40ba34293f3ed4eb81e4607d61b5165f7
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-27 11:11:12 +0300

    4

---
+++ Objects/Exercise/4.HeroicInventory.js
@@ -0,0 +1,14 @@
+function heroicInventory(list) {
+    let result = [];
+    for (let h of list) {
+        let [name, level, items] = h.split(' / ')
+        level = Number(level);
+        items = items ? items.split
+        (',').map(item => item.trim()) : [];
+        result.push({name, level, items});
+    }
+    console.log(JSON.stringify(result));
+}
+
+heroicInventory(['Isacc / 25 / Apple, GravityGun', 'Derek / 12 / BarrelVest, DestructionSword', 'Hes / 1 / Desolator, Sentinel, Antara']);
+heroicInventory(['Jake / 1000']);
