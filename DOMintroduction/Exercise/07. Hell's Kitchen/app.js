04467322f188310e90c35e0171ff36bc2c615c8a
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-05-28 17:57:13 +0300

    workers

--- DOMintroduction/Exercise/07. Hell's Kitchen/app.js
+++ DOMintroduction/Exercise/07. Hell's Kitchen/app.js
@@ -2,8 +2,12 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      let workers = document.getElementsByTagName('textarea')[0].value;
-      console.log(workers);
-      
+      let restourants = [];
+      let workers = JSON.parse(document.getElementsByTagName('textarea')[0].value.replace(' \g', ''));
+
+      for (let worker of workers) {
+         console.log(worker);
+         
+      }
    }
 }
