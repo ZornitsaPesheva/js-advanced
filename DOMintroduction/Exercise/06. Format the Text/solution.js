7d58cad3fbcaa3691cd815e07b767401b4af1ac4
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-05-28 13:02:20 +0300

    format

--- DOMintroduction/Exercise/06. Format the Text/solution.js
+++ DOMintroduction/Exercise/06. Format the Text/solution.js
@@ -1,13 +1,17 @@
 function solve() {
   let text = document.getElementById('input').value;
-  let seteces = text.split('.');
+
+  if (text.slice(-1) == '.')
+  text = text.slice(0, -1);
+  let setneces = text.split('.');
+
   let divElemet = document.getElementById('output');
-  for (let i = 0; i < seteces.length; i += 3) {
+  for (let i = 0; i < setneces.length; i += 3) {
     let p = document.createElement('p');
     p.textContent = '';
     for (j = i; j <= i + 3; j++){
-      if ( seteces[j] != undefined ){
-        p.textContent += `${seteces[j]}`;
+      if ( setneces[j] != undefined ){
+        p.textContent += `${setneces[j]}.`;
       }
     }
     divElemet.appendChild(p);
