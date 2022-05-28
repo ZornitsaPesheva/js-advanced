381fd744908be8c77f69bc0b7f4bbe84b75252b4
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-05-28 11:29:49 +0300

    search

--- DOMintroduction/Exercise/05. Table - Search Engine/solution.js
+++ DOMintroduction/Exercise/05. Table - Search Engine/solution.js
@@ -2,11 +2,11 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
+      let input = document.getElementById('searchField').value;
       let allTr = Array.from(document.querySelectorAll('tr'));
-      console.log(allTr);
-
       for (let tr of allTr){
          console.log(tr.textContent);
       }
+      console.log(input);
    }
 }
