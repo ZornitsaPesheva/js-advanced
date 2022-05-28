4b1c7d55c82a2a616344f76f29c2e14ba111a7f9
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-28 09:48:49 +0300

    Search in list

--- DOMintroduction/Exercise/04. Search in List/search.js
+++ DOMintroduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,13 @@
 function search() {
-   // TODO
+   let input = document.getElementById('searchText').value;
+   let resultList = document.getElementsByTagName('li')
+   let counter = 0;
+   for (let e of resultList) {
+      if (e.textContent.includes(input)) {
+         e.style.fontWeight = 'bold';
+         e.style.textDecoration = 'underline';
+         counter++
+      }
+      document.getElementById('result').textContent = `${counter} matches found`
+   }
 }
