a5b480e0850700c6a2608fc06e1de523718b5607
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-28 05:56:10 +0300

    collectItems

---
+++ DOMintroduction/Lab/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,5 @@
+function extractText() {
+    const items = Array.from(document.querySelectorAll('li'));
+    const result = items.map(e => e.textContent).join('\n');
+    document.getElementById('result').value = result;
+}
