a3cc84da0ba4dd68d567312aee4278ac237dac83
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-05-28 12:22:12 +0300

    hmmm

--- DOMintroduction/Exercise/06. Format the Text/solution.js
+++ DOMintroduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,10 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value;
+  let seteces = text.split('.');
+  let divElemet = document.getElementById('output');
+  for (let i = 0; i < seteces.length; i += 3) {
+    let p = document.createElement('p');
+    p.textContent = `${seteces[i]}${seteces[i+1]}${seteces[i+2]}`;
+    divElemet.append(p);
+  }
 }
