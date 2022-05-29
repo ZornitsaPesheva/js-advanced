bc6ba457d9a4923a8e9cf1d77ad9a21d771a0659
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-29 18:49:49 +0300

    event added opn btn

--- DOMintroduction/Exercise/09. Number Convertor/solution.js
+++ DOMintroduction/Exercise/09. Number Convertor/solution.js
@@ -1,11 +1,18 @@
 function solve() {
-    let optionBinary = document.createElement('option');
-    optionBinary.value = 'binary';
-    optionBinary.innerHTML = 'binary';
-    let optionDecimal = document.createElement('option');
-    optionDecimal.value = 'decimal';
-    optionDecimal.innerHTML = 'decimal';
-    let selectMenu = document.getElementById('selectMenuTo');
-    selectMenu.appendChild(optionBinary);
-    selectMenu.appendChild(optionDecimal);
+        let optionBinary = document.createElement('option');
+        optionBinary.value = 'binary';
+        optionBinary.innerHTML = 'binary';
+        let optionDecimal = document.createElement('option');
+        optionDecimal.value = 'decimal';
+        optionDecimal.innerHTML = 'decimal';
+        let selectMenu = document.getElementById('selectMenuTo');
+        selectMenu.appendChild(optionBinary);
+        selectMenu.appendChild(optionDecimal);
+
+        document.getElementsByTagName("button")[0].addEventListener('click', function () {
+            let decimal = document.getElementById('input').value;
+            console.log(decimal);
+        });
+        
+
 }
