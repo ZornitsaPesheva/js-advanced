7e1078f3af3ded88f3646914b3f226c536613bbd
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-29 11:02:11 +0300

    get elements for row

--- DOMintroduction/Exercise/08. Generate Report/report.js
+++ DOMintroduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,16 @@
 function generateReport() {
-    //TODO
+    let checked = [];
+    let inputList = document.querySelectorAll('th input');
+    for (let i = 0; i < inputList.length; i++) {
+        if (inputList[i].checked == true) {
+            checked.push(i)
+        }
+    }
+    console.log(checked);
+    let array = [];
+    let object = {};
+    let rows = document.getElementsByTagName('table')[0].rows.length;
+    for (let i = 0; i < rows; i++) {
+        console.log(`get ${checked} for row${i}`);
+    }
 }
