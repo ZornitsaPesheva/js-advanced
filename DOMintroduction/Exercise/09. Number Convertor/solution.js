e813116f64b854e10eebe63289f0a59f9dcc3a6d
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-29 18:36:19 +0300

    options added

--- DOMintroduction/Exercise/09. Number Convertor/solution.js
+++ DOMintroduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,11 @@
 function solve() {
-
-    //TODO...
+    let optionBinary = document.createElement('option');
+    optionBinary.value = 'binary';
+    optionBinary.innerHTML = 'binary';
+    let optionDecimal = document.createElement('option');
+    optionDecimal.value = 'decimal';
+    optionDecimal.innerHTML = 'decimal';
+    let selectMenu = document.getElementById('selectMenuTo');
+    selectMenu.appendChild(optionBinary);
+    selectMenu.appendChild(optionDecimal);
 }
