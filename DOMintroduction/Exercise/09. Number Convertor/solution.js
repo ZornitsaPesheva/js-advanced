f993882061b2487b2fa190b2e36f7c7dc20d2086
balkangraph <zornitsa.pesheva@outlook.com>
2022-05-30 07:01:12 +0300

    Heroes

--- DOMintroduction/Exercise/09. Number Convertor/solution.js
+++ DOMintroduction/Exercise/09. Number Convertor/solution.js
@@ -9,36 +9,18 @@ function solve() {
         selectMenu.appendChild(optionBinary);
         selectMenu.appendChild(optionHexaDecimal);
 
-        function convertToBinary(x) {
-            let bin = 0;
-            let rem, i = 1, step = 1;
-            while (x != 0) {
-                rem = x % 2;
-                x = parseInt(x / 2);
-                bin = bin + rem * i;
-                i = i * 10;
-            }
-
-            return bin;
-        }
-
-        function toHex(d) {
-            return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
-        }
-
-        let converted = '';
+        let converted = 0;
 
         document.getElementsByTagName("button")[0].addEventListener('click', function () {
-            let decimal = document.getElementById('input').value;         
+            let decimal = Number(document.getElementById('input').value);         
             let convertTo = selectMenu.value;
             if (convertTo == 'binary') {
-                converted = convertToBinary(decimal);
-                console.log(converted);
-                let result = document.getElementById('result');
+                converted = decimal.toString(2);
+                result = document.getElementById('result');
                 result.value = converted;
             }
             else if (convertTo == 'hexadecimal') {
-                converted = toHex(decimal);
+                converted = decimal.toString(16).toUpperCase();
                 let result = document.getElementById('result');
                 result.value = converted;
             }
