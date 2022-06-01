23e2d9e63ce6aefc760b5b8468f5d328fa562b2d
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-06-01 19:27:19 +0300

    mouse gradient

--- DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient =  document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMouseOver);
+    const result = document.getElementById('result');
+
+    function onMouseOver(ev) {
+      result.textContent = Math.floor(ev.offsetX / gradient.clientWidth * 100) + '%';
+    }
 }
