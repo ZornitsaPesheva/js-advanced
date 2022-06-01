09d8187872f2fd2921f11d0412d5372e9f8a299f
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-01 17:38:18 +0300

    add-delete

--- DOM Manipulations and Events/03. Add-Delete/app.js
+++ DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const content = document.getElementById('newItemText');
+    const listElement = document.createElement('li');
+    listElement.textContent = content.value;
+
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    listElement.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onDelete);
+
+    document.getElementById('items').appendChild(listElement);
+    content.value = '';
+    
+    function onDelete(e) {
+        e.target.parentElement.remove();
+    }
 }
