3dd6305ce4a681880bb867dd4dcdd9fa8963dea0
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-01 07:26:07 +0300

    2.delete form table

--- DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const value = document.querySelector('input[name="email"]').value;
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    let found = false;
+    for (let row of rows) {
+        if (row.children[1].textContent == value) {
+            const parent = row.parentElement;
+            parent.removeChild(row);
+            found = true;
+        }
+    }
+    if (found) {
+        document.getElementById('result').textContent = 'Deleted';
+    }
+    else {
+        document.getElementById('result').textContent = 'Not found.'
+    }
 }
