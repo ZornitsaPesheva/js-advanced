ee85893dcf94457afdbae8b730fb5a0325295727
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 13:33:04 +0300

    email validation

--- DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.querySelector('#email');
+    input.addEventListener('change', (event) => {
+        let value = event.target.value;
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))) {
+            input.classList.add('error');
+        }
+        else {
+            input.classList.remove('error');
+        }
+    });
 }
