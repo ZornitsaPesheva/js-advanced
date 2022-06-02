cd2e9a65def97c1fc22fd81a38323f438b68189a
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 15:56:31 +0300

    convertor

--- DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
+++ DOM Manipulations and Events/Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,42 @@
 function attachEventsListeners() {
+    let inputs = document.querySelectorAll('input');
 
-    console.log('TODO:...');
+    function convertToObject(value, time) {
+        let seconds = 0;
+        let object = {};
+        switch (time) {
+            case 'days': seconds = value * 24 * 60 * 60; break;
+            case 'hours': seconds = value * 60 * 60; break;
+            case 'minutes': seconds = value * 60; break;
+            case 'seconds': seconds = value; break;
+        }
+        object.seconds = seconds;
+        object.minutes = object.seconds / 60;
+        object.hours = object.minutes / 60;
+        object.days = object.hours / 24;
+        return object;
+    }
+
+    function convert(e){
+        let given = e.target.parentElement.querySelector('input');
+        let value = given.value;
+        let object = convertToObject(value, given.id);
+        for (let input of inputs) {
+            if (input.type == 'text' && input.id != given.id){
+                console.log(input.id);
+                switch (input.id) {
+                    case 'days': input.value = object.days; break;
+                    case 'hours': input.value = object.hours; break;
+                    case 'minutes': input.value = object.minutes; break;
+                    case 'seconds': input.value = object.seconds; break;
+                }
+            }
+        }
+    }
+
+    for (let input of inputs) {
+        if (input.type == 'button') {
+            input.addEventListener("click", convert);
+        }
+    }
 }
