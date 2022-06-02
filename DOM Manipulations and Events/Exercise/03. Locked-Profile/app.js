939320cede16056cc94e3d6064912484cb0e24a5
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 16:26:43 +0300

    profile

--- DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,14 +1,22 @@
 function lockedProfile() {
     let buttons = document.querySelectorAll('button');
-    function showMore(e) {
+    
+    function showHide(e) {
         let profile = e.target.parentElement;
+        let btn = profile.querySelector('button');
         let locked = profile.querySelector('div input').checked;
-        if (!locked) {
-            let more = profile.querySelector('div');
+        let more = profile.querySelector('div');
+        if (!locked && btn.textContent == 'Show more') {
             more.style.display = 'block';
+            btn.textContent = 'Hide it';
         }
+        else {
+            more.style.display = 'none';
+            btn.textContent = 'Show more';
+        }
+        
     }
     for (let button of buttons) {
-        button.addEventListener('click', showMore)
+        button.addEventListener('click', showHide);
     }
 }
