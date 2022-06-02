dc31d36305297abc357b82cf5c74c75a541fadd6
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 13:55:42 +0300

    shopping cart

--- DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-   //TODO...
+   let addButtons = document.querySelectorAll('button');
+   function addProduct(e){
+      console.log('add the product');
+
+      let product = e.target.parentElement.parentElement;
+      let name = product.querySelector('.product-title').textContent;
+      let money = product.querySelector('.product-line-price').textContent;
+      let textArea = document.querySelector('textarea');
+      textArea.textContent += `Added ${name} for ${money} to the cart.\n`;
+    //  e.target.removeEventListener('click', addProduct);
+   }
+   for (let button of addButtons) {
+      button.addEventListener('click', addProduct);
+   }
 }
