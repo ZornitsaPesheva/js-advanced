e36a2a5ea8a41e87dc600ce8586e5ec2cfaa9527
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 14:11:07 +0300

    checkout remove listeners

--- DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,16 +1,26 @@
 function solve() {
-   let addButtons = document.querySelectorAll('button');
+   let addButtons = document.querySelectorAll('.add-product');
+   let total = 0;
    function addProduct(e){
-      console.log('add the product');
-
       let product = e.target.parentElement.parentElement;
       let name = product.querySelector('.product-title').textContent;
       let money = product.querySelector('.product-line-price').textContent;
+      total += money;
       let textArea = document.querySelector('textarea');
       textArea.textContent += `Added ${name} for ${money} to the cart.\n`;
-    //  e.target.removeEventListener('click', addProduct);
    }
+
+   function checkOut(e) {
+      for (let button of addButtons) {
+         button.removeEventListener('click', addProduct);
+      }
+   }
+
    for (let button of addButtons) {
       button.addEventListener('click', addProduct);
    }
+
+   let checkout = document.querySelector('.checkout');
+   checkout.addEventListener('click', checkOut);
+
 }
