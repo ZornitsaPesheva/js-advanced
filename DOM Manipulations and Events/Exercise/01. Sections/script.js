64c916b2397d71693c2ae76f5e65e5463271208f
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 15:17:35 +0300

    Sections

--- DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,13 @@
 function create(words) {
-   console.log('TODO:...');
+   for (let word of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.style.display = 'none';
+      div.appendChild(p);
+      p.textContent = word;
+      document.querySelector('#content').appendChild(div);
+      div.addEventListener('click', function() {
+         p.style.display = 'block';
+      });
+   }
 }
