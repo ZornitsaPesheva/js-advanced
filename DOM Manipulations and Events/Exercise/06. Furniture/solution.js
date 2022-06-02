105ec972cac6c84a59a468e9f5ef4552daae3041
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 18:28:19 +0300

    furniture

--- DOM Manipulations and Events/Exercise/06. Furniture/solution.js
+++ DOM Manipulations and Events/Exercise/06. Furniture/solution.js
@@ -1,8 +1,24 @@
 function solve() {
   function addFurniture(){
-    let array = document.querySelector('textarea').value;
-    console.log(array);
+    let value = document.querySelector('textarea').value;
+    let array = JSON.parse(value);
 
+    for (let object of array) {
+      console.log(object.img);
+      let tr = document.createElement('tr');
+      let tdImage = document.createElement('td');
+      let img = document.createElement('img');
+      img.src = object.img;
+      tdImage.appendChild(img);
+      tr.appendChild(tdImage);  
+      let tdName = document.createElement('td');
+      let p = document.createElement('p');
+      p.textContent = object.name;
+      tdName.appendChild(p);
+      tr.appendChild(tdName); 
+      document.querySelector('table').appendChild(tr);
+    }
+    console.log('added');
   }
 
  document.querySelector('button').addEventListener('click', addFurniture)
