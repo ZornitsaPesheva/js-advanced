a0dfa33864baa4c86297f313c67f19dcb498a5dd
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 17:25:29 +0300

    ecode/decode - 50

--- DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -11,16 +11,16 @@ function encodeAndDecodeMessages() {
         receiver.value = encoded;
     }
     function decode(e) {
-        // let message = document.querySelector('#main textarea').value;
-        // let encoded = '';
-        // for (symbol in message) {
-        //     let i = symbol.charCodeAt(0) + 1;   
-        //     encoded += symbol;
-        // }
-        // console.log(encoded);
+        let message = document.querySelectorAll('#main textarea')[1].value;
+        let array = [];
+        for (let symbol of message) {
+            let i = symbol.charCodeAt(0);   
+            array.push(i - 1);
+        }
+        let decoded = String.fromCharCode(...array);
+        let receiver = document.querySelectorAll('#main textarea')[1];
+        receiver.value = decoded;
     }
     document.querySelector('button').addEventListener('click', encode);
     document.querySelectorAll('button')[1].addEventListener('click', decode);
-
-
 }
