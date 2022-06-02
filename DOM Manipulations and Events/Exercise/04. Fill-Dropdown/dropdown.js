9a098f547cbe8d00d53e342c4396d1247e208618
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-02 16:58:56 +0300

    04. Fill Dropdown

--- DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText').value;
+    let value = document.getElementById('newItemValue').value;
+    let option = document.createElement('option');
+    option.textContent = text;
+    option.value = value;
+    let select = document.getElementById('menu');
+    select.appendChild(option);
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = ''
 }
