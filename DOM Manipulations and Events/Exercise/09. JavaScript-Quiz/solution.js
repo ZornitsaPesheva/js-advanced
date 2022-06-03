b94f0f8442247f796db22d89b4a51c8df2848f37
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-03 16:00:43 +0300

    classlist

--- DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,16 +1,19 @@
 function solve() {
     let [section1, section2, section3] = document.querySelectorAll('section');
     let answers = document.querySelectorAll('.answer-text');
-    let [answer1, answer2, answer3] = answers;
+    let [answer1, answer2, answer3, awswer4, answer5, answer6 ] = answers;
     function showNext(e) {
-      if (e.targed != answer3) {
-        let section = e.target.parentElement.parentElement.parentElement.parentElement;
-        section.classList.add('hidden');
-        let nextSection = section.nextElementSibling;
-        console.log(section);
-
+      let section = e.target.parentElement.parentElement.parentElement.parentElement;
+      let nextSection = section.nextElementSibling;
+      section.classList.add('hidden');
+      if (e.target != answer5 && e.target != answer6) {
         nextSection.classList.remove('hidden');
-
+      }
+      else {
+        let results = document.querySelector('#results h1');
+        results.textContent = "You are recognized as top JavaScript fan!";
+        console.log(results.style);
+        document.querySelector('#results').classList.add('current');
       }
     }
     for (let answer of answers) {
