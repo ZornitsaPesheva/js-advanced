981929ea38a9cf5f03f9c5ae233f2d6e2a061045
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-03 16:23:50 +0300

    09. JavaScript Quizz 100

--- DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,20 +1,34 @@
 function solve() {
     let [section1, section2, section3] = document.querySelectorAll('section');
     let answers = document.querySelectorAll('.answer-text');
-    let [answer1, answer2, answer3, awswer4, answer5, answer6 ] = answers;
+    let [answer1, answer2, answer3, answer4, answer5, answer6 ] = answers;
+    let correctAnswers = [];
+    let countCorrect = 0;
     function showNext(e) {
       let section = e.target.parentElement.parentElement.parentElement.parentElement;
       let nextSection = section.nextElementSibling;
-      section.classList.add('hidden');
+      let results = document.querySelector('#results h1');
+      section.style.display = 'none';
+      if ([answer1, answer4, answer5].includes(e.target)) {
+        countCorrect++;
+        correctAnswers.push(e.target);
+
+      }
       if (e.target != answer5 && e.target != answer6) {
-        nextSection.classList.remove('hidden');
+        nextSection.style.display = 'block';
+      }
+     
+      else if (correctAnswers.includes(answer1) &&
+          correctAnswers.includes(answer4) &&
+          correctAnswers.includes(answer5)){
+        results.textContent = "You are recognized as top JavaScript fan!";
+        document.querySelector('#results').style.display = 'block';
       }
       else {
-        let results = document.querySelector('#results h1');
-        results.textContent = "You are recognized as top JavaScript fan!";
-        console.log(results.style);
-        document.querySelector('#results').classList.add('current');
+        results.textContent = `You have ${countCorrect} right answers`;
+        document.querySelector('#results').style.display = 'block';
       }
+
     }
     for (let answer of answers) {
       answer.addEventListener('click', showNext);
