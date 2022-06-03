4d8c4bd8683396bf01d224ccb87fb018d643c670
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-03 08:51:28 +0300

    quiz

--- DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO...
+    let [section1, section2, section3] = document.querySelectorAll('section');
+    let answers = document.querySelectorAll('.answer-text');
+    let [answer1, answer2, answer3] = answers;
+    function showNext(e) {
+      if (e.targed != answer3) {
+        let section = e.target.parentElement.parentElement.parentElement.parentElement;
+        section.classList.add('hidden');
+        let nextSection = section.nextElementSibling;
+        console.log(section);
+
+        nextSection.classList.remove('hidden');
+
+      }
+    }
+    for (let answer of answers) {
+      answer.addEventListener('click', showNext);
+    }
 }
