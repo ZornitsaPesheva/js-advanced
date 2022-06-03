56a559d51b1fb986f164e8ad4a208f1a927ef19a
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-03 06:48:35 +0300

    sudomu

--- DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
+++ DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    let checkBtn = document.querySelector('button');
 
-    //TODO...
+    function check() {
+        let firstRow = document.querySelectorAll('tr')[2];
+        let secondRow = document.querySelectorAll('tr')[3];
+        let tirthRow = document.querySelectorAll('tr')[4];
+        let firsRowValues = firstRow.querySelectorAll('td input');
+        let sum = Number(firsRowValues[0].value) 
+            + Number(firsRowValues[1].value) 
+            + Number(firsRowValues[2].value);
+        let firstColumnSum = 
+            Number(firstRow.querySelectorAll('td input')[0].value)
+            + Number(secondRow.querySelectorAll('td input')[0].value)
+            + Number(tirthRow.querySelectorAll('td input')[0].value);
+        let secondColumnSum = 
+            Number(firstRow.querySelectorAll('td input')[1].value)
+            + Number(secondRow.querySelectorAll('td input')[1].value)
+            + Number(tirthRow.querySelectorAll('td input')[1].value);
+        let tirthColumnSum = 
+            Number(firstRow.querySelectorAll('td input')[2].value)
+            + Number(secondRow.querySelectorAll('td input')[2].value)
+            + Number(tirthRow.querySelectorAll('td input')[2].value);
+    }
+
+    checkBtn.addEventListener('click', check);
 }
