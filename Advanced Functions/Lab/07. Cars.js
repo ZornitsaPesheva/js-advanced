fd6d26e56c6d8abe659a85d4edbf1694fe33b2e2
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-04 19:48:16 +0300

    cars from Kris

--- Advanced Functions/Lab/07. Cars.js
+++ Advanced Functions/Lab/07. Cars.js
@@ -1,73 +1,42 @@
-function createModify(input) {
-    const cars = [];
+//from Kris
 
-
-    function inheritCar(oldCar, newCar) {
-
-    }
-
-    let r = {
-        create: (rest) => {
-            if (rest.length == 1) {
-                let car = {};
-                car.name = rest[0];
-                cars.push(car);
-            }
-            else {
-                let oldCar = cars.find(c => c.name == rest[2]) 
-                let car = Object.assign({}, oldCar);
-                car.name = rest[0];
-                car.inherit = rest[2];
-                cars.push(car);
-            }
-            
+function solve(input) {
+    const data = {};
+ 
+    const commandsMap = {
+        create: (name, inherits, parentName) => {
+            data[name] = inherits ? Object.create(data[parentName]) : {};
         },
-
-        set: (rest) => {
-            let car = cars.find(c => c.name == rest[0]);
-            let property = rest[1];
-            car[property] = rest[2];
-        },
-
-        print: (rest) => {
-            let car = cars.find(c => c.name == rest[0]);
-            let result = [];
-            let inheritance = cars.find(c => c.inherit != undefined);
-            let props = [];
-            for (const prop in car) {
-                if (inheritance) {
-                    let inheritedCar = cars.find(c => c.name == car[prop]);
-                    for (const p in inheritedCar) {
-                        if (p != 'name' && p != 'inherit') {
-                            result.push(`${p}:${inheritedCar[p]}`);
-                            props.push(p);
-                        }
-                    }
-                }
-                if (prop != 'name' && prop != 'inherit') {
-                    if (!props.includes(prop)) {
-                        result.push(`${prop}:${car[prop]}`);
-                    }
-                }
+        set: (name, k, v) => data[name][k] = v,
+        print: name => {
+            const entries = [];
+            for (const key in data[name]) {
+                entries.push(`${key}:${data[name][key]}`);
             }
-            console.log(result.join(','))
-        }
-
+            console.log(entries.join(','));
+        },
     }
-
-    input.forEach(el => {
-        let [command, ...rest] = el.split(' ');
-        result = r[command](rest);
-        
+ 
+    input.forEach(x => {
+        const [command, name, k, v] = x.split(" ");
+        commandsMap[command](name, k, v);
     });
-
 }
 
+// createModify([
+//     'create c1',
+//     'create c2 inherit c1',
+//     'set c1 color red',
+//     'set c2 model new',
+//     'print c1',
+//     'print c2'
+// ]);
+
 createModify([
-    'create c1',
-    'create c2 inherit c1',
-    'set c1 color red',
-    'set c2 model new',
-    'print c1',
-    'print c2'
-]);
+    'create pesho',
+    'create gosho inherit pesho',
+    'create stamat inherit gosho',
+    'set pesho rank number1',
+    'set gosho nick goshko',
+    'print stamat'
+])
