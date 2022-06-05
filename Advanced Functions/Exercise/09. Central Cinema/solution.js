0d2d8f7cf0d01c40deb365a63426a950cdd776a5
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-05 18:05:20 +0300

    smtng like this

--- Advanced Functions/Exercise/09. Central Cinema/solution.js
+++ Advanced Functions/Exercise/09. Central Cinema/solution.js
@@ -1,3 +1,35 @@
 function solve() {
-    // TODO
+
+    function addMovie(inputs) {
+        [mName, hall, price] = inputs;
+        let span = document.createElement('span');
+        span.textContent = mName;
+        let strong = document.createElement('strong');
+        strong.textContent = hall;
+        
+        let strongInDiv = document.createElement('strong');
+        strongInDiv.textContent = Number(price); 
+        let input = document.createElement('input');
+        input.setAttribute('placeholder', 'Tickets Sold');
+        let btn = document.createElement('button');
+        btn.textContent = 'Archive';
+        let div = document.createElementDiv('div');
+        div.appendChild(strongInDiv);
+        div.appendChild(input);
+        div.appendChild(btn);
+
+        let ul = document.querySelector('section ul');
+        ul.appendChild(span);
+        ul.appendChild(strong);
+        ul.appendChild(div);
+    }
+
+    let inputs = document.querySelectorAll('input');
+
+    if (![...inputs].some(i => i.value == '')) {
+        let onScreen = document.getElementsByTagName('button');
+        onScreen.addEventListener('click', addMovie);
+    }
+
+    
 }
