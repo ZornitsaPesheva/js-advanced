67bf16df75c865f6b18302609418f38376edbfe9
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-05 19:43:17 +0300

    not working

--- Advanced Functions/Exercise/09. Central Cinema/solution.js
+++ Advanced Functions/Exercise/09. Central Cinema/solution.js
@@ -1,35 +1,52 @@
 function solve() {
 
-    function addMovie(inputs) {
-        [mName, hall, price] = inputs;
-        let span = document.createElement('span');
-        span.textContent = mName;
-        let strong = document.createElement('strong');
-        strong.textContent = hall;
-        
-        let strongInDiv = document.createElement('strong');
-        strongInDiv.textContent = Number(price); 
-        let input = document.createElement('input');
-        input.setAttribute('placeholder', 'Tickets Sold');
-        let btn = document.createElement('button');
-        btn.textContent = 'Archive';
-        let div = document.createElementDiv('div');
-        div.appendChild(strongInDiv);
-        div.appendChild(input);
-        div.appendChild(btn);
-
-        let ul = document.querySelector('section ul');
-        ul.appendChild(span);
-        ul.appendChild(strong);
-        ul.appendChild(div);
-    }
-
     let inputs = document.querySelectorAll('input');
 
-    if (![...inputs].some(i => i.value == '')) {
-        let onScreen = document.getElementsByTagName('button');
-        onScreen.addEventListener('click', addMovie);
+    const [mName, hall, price] = inputs;
+
+    console.log([mName, hall, price]);
+
+    let onScreenBtn= document.getElementsByTagName('button')[0];
+
+
+    onScreenBtn.addEventListener('click', addMovie);
+
+    function addMovie() {
+        e.preventDefault();
+        // console.log(document.querySelectorAll('input'));
+
+
+        // console.log(inputs);
+
+        // console.log(mName.value);
+         if (mName.value != '' && hall.value != '' && price.value != '' && !isNaN(Number(price.value))) {
+
+            let span = document.createElement('span');
+            span.textContent = mName;
+            let strong = document.createElement('strong');
+            strong.textContent = hall;
+            
+            let strongInDiv = document.createElement('strong');
+            strongInDiv.textContent = Number(price.value).toFixed(2); 
+            let input = document.createElement('input');
+            input.setAttribute('placeholder', 'Tickets Sold');
+            let btn = document.createElement('button');
+            btn.textContent = 'Archive';
+            let div = document.createElement('div');
+            div.appendChild(strongInDiv);
+            div.appendChild(input);
+            div.appendChild(btn);
+    
+            let ul = document.querySelector('section ul');
+            let li = document.createElement('li');
+            li.appendChild(span);
+            li.appendChild(strong);
+            li.appendChild(div);
+            ul.appendChild(li);
+        }
+
     }
+
 
     
 }
