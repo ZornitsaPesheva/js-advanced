e5098d4a64b8e79c74a600cba8e7743e5f57ef8f
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-05 15:27:56 +0300

    robot 100

--- Advanced Functions/Exercise/04. Breakfast Robot.js
+++ Advanced Functions/Exercise/04. Breakfast Robot.js
@@ -28,29 +28,37 @@ function solution() {
     }
 
     const storage = {
+        protein: 0,
         carbohydrate: 0,
-        flavour: 0,
         fat: 0,
-        protein: 0
+        flavour: 0        
     }
 
     const func = {
         restock: (arg1, arg2) => {
-            storage[arg1] += arg2;
+            storage[arg1] += Number(arg2);
             return `Success`;
         },
         prepare: (arg1, arg2) => {
-
+            let result = '';
             let recipe = recipes[arg1];
             for (let r in recipe) {
-                let outOfStorage = recipe[r] * arg1;
+                let outOfStorage = recipe[r] * arg2;
                 if (storage[r] > outOfStorage) {
-                    return 'Success'
+                    storage[r] -= outOfStorage;
+                    result = 'Success'
                 }
                 else {
                     return `Error: not enough ${r} in stock`;
                 }
             }
+            return result;
+        },
+        report: () => {
+            let array = [];
+            Object.keys(storage)
+            .forEach((key) => array.push(`${key}=${storage[key]}`));
+            return array.join(' ');
         }
     }
 
@@ -65,11 +73,10 @@ function solution() {
 
 let manager = solution();
 
-console.log(manager("'restock carbohydrate 10")); // Success
+console.log(manager("restock flavour 50")); // Success
 
-console.log(manager("restock flavour 10")); // Error: not enough carbohydrate in
-
-console.log(manager("prepare apple 1"));
-console.log(manager("restock fat 10"));
-console.log(manager("prepare burger 1"));
-console.log(manager('report', 'protein=0 carbohydrate=4 fat=3 flavour=5'));
+console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in
+//console.log(manager("prepare apple 1"));
+//  console.log(manager("restock fat 10"));
+//  console.log(manager("prepare burger 1"));
+  console.log(manager('report'));
