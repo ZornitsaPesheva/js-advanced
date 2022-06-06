08c896115c48e31f8f02782764f5dbd228f77719
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-06 08:17:36 +0300

    archive

--- Advanced Functions/Exercise/09. Central Cinema/solution.js
+++ Advanced Functions/Exercise/09. Central Cinema/solution.js
@@ -4,6 +4,9 @@ function solve() {
     const [mName, hall, price] = inputs;
     let onScreenBtn= document.getElementsByTagName('button')[0];
     onScreenBtn.addEventListener('click', addMovie);
+    let ulElement = document.querySelector('ul');
+    let ulElement2 = document.querySelectorAll('ul')[1];
+
 
     function addMovie(e) {
         e.preventDefault();
@@ -31,10 +34,29 @@ function solve() {
             li.appendChild(strong);
             li.appendChild(div);
             ul.appendChild(li);
+            mName.value != ''; hall.value != ''; price.value != ''
+            btn.addEventListener('click', (e) => archive(e, input.value, mName,price));
+        }
+
+        function archive(e,value, mName){
+            if (value !== ""){
+                e.target.parentNode.parentNode.remove();
+                let liElement = creatingElements('li', "", ulElement2);
+                creatingElements('span', mName.value, liElement)
+                creatingElements('strong', `Total amount: ${(price.value*value).toFixed(2)}`, liElement);
+                let deleteButton = creatingElements('button', 'Delete', liElement)
+                deleteButton.addEventListener('click', (e) => e.target.parentNode.parentNode.remove())
+            }
+        }
+
+        function creatingElements(elementToCreate, textContent, appendTo){
+            let newElement = document.createElement(elementToCreate);
+            newElement.textContent = textContent;
+            if (appendTo){
+                appendTo.appendChild(newElement);
+            }
+            return newElement
         }
 
     }
-
-
-    
 }
