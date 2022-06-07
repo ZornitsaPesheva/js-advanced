488c85ccdac638e7715952450c357c46d42a280b
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-07 12:46:18 +0300

    post for review

--- Exams/Retake Exam - 6 Apr 2022/Forum Posts/app.js
+++ Exams/Retake Exam - 6 Apr 2022/Forum Posts/app.js
@@ -1,5 +1,36 @@
 window.addEventListener("load", solve);
 
 function solve() {
+  const publishBtn = document.getElementById('publish-btn');
+  const input = {
+    title: document.getElementById('post-title'),
+    category: document.getElementById('post-category'),
+    content: document.getElementById('post-content')
+  } 
+  let review = document.getElementById('review-list');
+  publishBtn.addEventListener('click', publish);
+
+  function publish() {
+    if (input.title.value != '' && input.category.value != '' && input.content.value != '') {
+      let li = document.createElement('li');
+      review.appendChild(li);
+      let article = createElement('article');
+      li.appendChild(article);
+      li.appendChild(createElement('button', 'Edit', 'action-btn edit'));
+      li.appendChild(createElement('button', 'Edit', 'action-btn approve'));
+      article.appendChild(createElement('h4', input.title.value));
+      article.appendChild(createElement('p', 'Category ' + input.category.value));
+      article.appendChild(createElement('h4', 'Content: ' + input.content.value));
+    }
+  }
+
+  function createElement(type, content, className) {
+    const element = document.createElement(type);
+    element.textContent = content;
+    if (className) {
+        element.className = className;
+    }
+    return element;
+}
   //TODO ...
 }
