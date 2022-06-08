3ce6a27d79be4b0fa4edc92931c09882db00b107
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-06-08 17:26:48 +0300

    06. RGB to Hex

--- Unit Testing and Error Handling/Lab/06. RGB to Hex/rgb.test.js
+++ Unit Testing and Error Handling/Lab/06. RGB to Hex/rgb.test.js
@@ -14,10 +14,32 @@ describe('RGBtoHexColor', () => {
         expect(rgbToHexColor(35, 68, 101)).to.equal('#234465');
     });
 
+    it ('converts 15, 15, 15  to #0F0F0F', () => {
+        expect(rgbToHexColor(15, 15, 15)).to.equal('#0F0F0F');
+    });
+
     it ('return undefined for missing params', () => {
-        expect(rgbToHexColor(30, 0)).to.be.undefined;
-        expect(rgbToHexColor(30, 0)).to.be.undefined;
-        expect(rgbToHexColor(30, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0)).to.be.undefined;
+        expect(rgbToHexColor(0)).to.be.undefined;
+    });
+
+    it ('return undefined for out of lower band', () => {
+        expect(rgbToHexColor(-1, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, -1, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, -1)).to.be.undefined;
+    });
+
+    it ('return undefined for out of upper band', () => {
+        expect(rgbToHexColor(256, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 256, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, 256)).to.be.undefined;
+    });
+
+    it ('return undefined for string', () => {
+        expect(rgbToHexColor('1', 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, '1', 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, '1')).to.be.undefined;
     });
 
 });
