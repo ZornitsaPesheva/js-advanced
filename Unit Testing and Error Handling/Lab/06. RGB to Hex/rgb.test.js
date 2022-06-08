5ade121b50a41b12588c726d11644dce63d676b4
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-08 14:37:07 +0300

    doble

--- Unit Testing and Error Handling/Lab/06. RGB to Hex/rgb.test.js
+++ Unit Testing and Error Handling/Lab/06. RGB to Hex/rgb.test.js
@@ -1,2 +1,16 @@
-const ( expect ) = require('chai');
-const { rgbToHexColor } = require('./rgb')
+const { expect } = require('chai');
+const { rgbToHexColor } = require('./rgb');
+
+describe('RGBtoHexColor', () => {
+    if ('converts black', () => {
+        expect(rgbToHexColor(0, 0, 0)).to.equal('#000000');
+    });
+
+    if ('converts white', () => {
+        expect(rgbToHexColor(255, 255, 255)).to.equal('#FFFFFF');
+    });
+
+    if ('converts SoftUni blue to #234465', () => {
+        expect(rgbToHexColor(255, 255, 255)).to.equal('#FFFFFF');
+    });
+});
