7c00f65e583aef323c6a21cd271e3359c6e20b95
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-08 13:34:51 +0300

    7 tests

--- Unit Testing and Error Handling/Lab/05. Check for Symmetry/symmetry.test.js
+++ Unit Testing and Error Handling/Lab/05. Check for Symmetry/symmetry.test.js
@@ -4,17 +4,29 @@ const { isSymmetric } = require('./symmetry');
 describe('Symetry Checker', () => {
     it ('works with symetric numeric numeric array', () => {
         expect(isSymmetric([1, 2, 2, 1])).to.be.true;
-    })
+    });
 
     it ('returns false for non-symetric numeric array', () => {
         expect(isSymmetric([1, 2, 3])).to.be.false;
-    })
+    });
 
     it ('returns false for non array', () => {
         expect(isSymmetric(5)).to.be.false;
-    })
+    });
 
-    if ('works with symetric odd-length array', () => {
-        expect(isSymmetric(['a', 'b', 'b', 'ba'])).to.be.true;
-    })
-})
+    it ('works with symetric odd-length array', () => {
+        expect(isSymmetric([1, 2, 1])).to.be.true;
+    });
+
+    it ('works with symetric string array', () => {
+        expect(isSymmetric('a', 'b', 'b', 'a')).to.be.false;
+    });
+
+    it ('returns false for string param', () => {
+        expect(isSymmetric('abba')).to.be.false;
+    });
+
+    it ('returns false for type mismatched elements', () => {
+        expect(isSymmetric([1, 2, '1'])).to.be.false;
+    });
+});
