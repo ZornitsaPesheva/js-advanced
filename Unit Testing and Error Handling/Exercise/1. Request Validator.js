bb5f5eb10d4b34999a1a9a49832638b6c0b130d4
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-09 11:14:36 +0300

    uri

--- Unit Testing and Error Handling/Exercise/1. Request Validator.js
+++ Unit Testing and Error Handling/Exercise/1. Request Validator.js
@@ -1,8 +1,14 @@
 function validate({method, uri, verion, message}) {
-    const methods = ['GET', 'POST', 'DELETE', 'CONNECT']
+    const methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+
     if (!methods.includes(method)) {
         throw new Error(`Invalid request header: Invalid ${method}`)
     }
 
-    if(m)
+
+    if (uri != new RegExp('[A-z0-9]*\.[A-z0-9]+')) {
+        throw new Error(`Invalid request header: Invalid ${uri}`)
+    }
+
+
 }
