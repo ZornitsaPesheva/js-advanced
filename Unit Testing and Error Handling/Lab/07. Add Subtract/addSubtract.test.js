c163c2afad59c0b948fb91671f5eb3a2a94e7fcc
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-09 10:36:14 +0300

    add sustract

--- Unit Testing and Error Handling/Lab/07. Add Subtract/addSubtract.test.js
+++ Unit Testing and Error Handling/Lab/07. Add Subtract/addSubtract.test.js
@@ -3,43 +3,23 @@ const { createCalculator } = require('./addSubtract');
 
 
 describe('Add / Subtractr', () => {
+
     it ('check whether it returns properties are functions', () => {
-        expect(createCalculator()).to.haveOwnProperty('add').to.be.a('function');
-        expect(createCalculator()).to.haveOwnProperty('subtract').to.be.a('function');
-        expect(createCalculator()).to.haveOwnProperty('get');
+        expect(typeof createCalculator().add).to.equal('function');
+        expect(typeof createCalculator().subtract).to.equal('function');
+        expect(typeof createCalculator().get).to.equal('function');
     });
 
-    it ('check if add and subtract work with numbers', () => {
-        let calc = createCalculator();
-        calc.add(5,5);
-        calc.subtract(5,5);
-        expect(calc.get()).to.be.a('number');
-    });
 
     it ('check if add and subtract calsulate properly', () => {
         let calc = createCalculator();
-        calc.add(5,5);
-        calc.subtract(5,5);
-        expect(calc.get()).to.equal(0);
+        calc.add('5');
+        expect(calc.get()).to.equal(5);
     });
 
     it ('check if add and subtract get only numbers', () => {
         let calc = createCalculator();
         calc.add('g');
-        calc.subtract('s');
-        expect(calc.get()).to.be.NaN;
-    });
-
-    it ('check if add and subtract work with parsed numbers', () => {
-        let calc = createCalculator();
-        calc.add('string');
-        expect(calc.get()).to.be.NaN;
-    });
-
-    it ('not work without parameters', () => {
-        let calc = createCalculator();
-        calc.add();
-        calc.subtract();
         expect(calc.get()).to.be.NaN;
     });
 
@@ -56,9 +36,6 @@ describe('Add / Subtractr', () => {
     it ('returns an object', () => {
         let calc = createCalculator();
         expect(calc).to.be.a('object');
-        expect(calc).to.haveOwnProperty('add').to.equal('function');
-        expect(calc).to.haveOwnProperty('subtract').to.equal('function');
-        expect(calc).to.haveOwnProperty('get').to.equal('function');
     });
 
 });
