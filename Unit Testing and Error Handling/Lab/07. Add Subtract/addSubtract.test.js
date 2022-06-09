bf20224857f0514e99ae0ba0f53b443f5436aa4f
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-09 08:19:02 +0300

    71

--- Unit Testing and Error Handling/Lab/07. Add Subtract/addSubtract.test.js
+++ Unit Testing and Error Handling/Lab/07. Add Subtract/addSubtract.test.js
@@ -4,30 +4,31 @@ const { createCalculator } = require('./addSubtract');
 
 describe('Add / Subtractr', () => {
 
-    let calc = createCalculator();
 
     it ('check whether it returns properties are functions', () => {
         expect(createCalculator()).to.haveOwnProperty('add').to.be.a('function');
         expect(createCalculator()).to.haveOwnProperty('subtract').to.be.a('function');
-        expect(createCalculator()).to.haveOwnProperty('get').to.be.a('function');
+        expect(createCalculator()).to.haveOwnProperty('get');
     });
 
     it ('check if add and subtract work with numbers', () => {
-        calc.add(5);
-        calc.subtract(5);
+        let calc = createCalculator();
+        calc.add(5,5);
+        calc.subtract(5,5);
         expect(calc.get()).to.be.a('number');
     });
 
     it ('check if add and subtract get only numbers', () => {
+        let calc = createCalculator();
         calc.add('g');
         calc.subtract('s');
         expect(calc.get()).to.be.NaN;
     });
 
     it ('check if add and subtract work with parsed numbers', () => {
-        calc.add(Number('6'));
-        calc.subtract(Number('6'));
+        let calc = createCalculator();
+        calc.add(Number('6,5'));
+        calc.subtract(Number('6,5'));
         expect(calc.get()).to.be.a('number');
     });
-
 });
