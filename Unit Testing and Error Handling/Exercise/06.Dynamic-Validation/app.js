9925218bfed67544ddcd3c39d239425576a0c076
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-09 19:49:32 +0300

    06. Dynamic Validation

--- Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
+++ Unit Testing and Error Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let emailEl = document.getElementById('email');
+    let pattern = new RegExp('^[a-z]+@[a-z]+\\.[a-z]+');
+
+    emailEl.addEventListener('change', () => {
+        if (pattern.test(emailEl.value)) {
+            emailEl.classList.remove('error')
+        }
+        else {
+            emailEl.classList.add('error')
+        }
+    })
 }
