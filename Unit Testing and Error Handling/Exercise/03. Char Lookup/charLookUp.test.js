4ba42288e05195ebe2e05cc439c9e1cbda394100
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-09 19:13:33 +0300

    03. Char Lookup

--- Unit Testing and Error Handling/Exercise/03. Char Lookup/charLookUp.test.js
+++ Unit Testing and Error Handling/Exercise/03. Char Lookup/charLookUp.test.js
@@ -1,4 +1,34 @@
 const { expect } = require('chai');
 const { lookupChar } = require('./charLookUp')
 
+describe('Look Up char', () => {
+
+    it ('check whether first parameter is valid', () => {
+        expect(lookupChar(1, 1)).to.equal(undefined);
+    });
+
+    it ('check whether second parameter is valid', () => {
+        expect(lookupChar('hello', 'test')).to.equal(undefined);
+    });
+
+    it ('check whether second parameter is valid integer', () => {
+        expect(lookupChar('hello', 0.1)).to.equal(undefined);
+    });
+
+    it ('check value with index bigger than string length', () => {
+        expect(lookupChar('hi', 10)).to.equal('Incorrect index');
+    });
+
+    it ('check value with negative index', () => {
+        expect(lookupChar('hi', -10)).to.equal('Incorrect index');
+    });
+
+    it ('check value with valid params', () => {
+        expect(lookupChar('hello', 1)).to.equal('e');
+    });
+
+});
+
+
+
 
