2646234303cc8ec7271bb88fa2d4603dbb3e47da
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-10 08:10:35 +0300

    x

---
+++ Exams/13 March 2022/03. Rent Car/rentCar.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const { rentCar } = require('./rentCar');
+
+describe('Rent Car', function() {
+    describe('searchCar', function() {
+        it ('return correct data whether the model is in the shop', () => {
+            expect(rentCar.searchCar(["Volkswagen", "BMW", "Audi"], 'Volkswagen'))
+            .to.deep.equal('There is 1 car of model Volkswagen in the catalog!')
+        })
+
+        it ('check if shop data is array of strings', () => {
+            expect(rentCar.searchCar('fdgdf', 'Volkswagen')).to.be.undefined;
+        })
+    });
+
+    describe('calculatePriceOfCar', function() {
+
+    });
+    
+    describe('checkBudget', function() {
+
+    });
+});
