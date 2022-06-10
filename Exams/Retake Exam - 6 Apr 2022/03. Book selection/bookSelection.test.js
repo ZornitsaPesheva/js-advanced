b437427a28cea6a2b5e338acacc5c01dba243793
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-10 06:53:06 +0300

    2

--- Exams/Retake Exam - 6 Apr 2022/03. Book selection/bookSelection.test.js
+++ Exams/Retake Exam - 6 Apr 2022/03. Book selection/bookSelection.test.js
@@ -3,14 +3,23 @@ const { bookSelection } = require('./bookSelection');
 
 describe('Book Selection', function() {
     describe('isGenreSuitable', function() {
-        it ('', () => {
-            expect(bookSelection.isGenreSuitable('man', 5)).to.equal('Those books are suitable');
+        it ('Fairy Tales are sutable for 5 years old.', () => {
+            expect(bookSelection.isGenreSuitable('Fairy Tales', 5)).to.equal('Those books are suitable');
         })
-    })
+        it ('Thrillers are not sutable for 5 years old.', () => {
+            expect(bookSelection.isGenreSuitable('Thriller', 5)).to.equal('Books with Thriller genre are not suitable for kids at 5 age');
+        })
+    });
 
     describe('isItAffordable', function() {
-    })
+        it ('Book is affordable.', () => {
+            expect(bookSelection.isItAffordable(15, 20)).to.equal('Book bought. You have 5$ left');
+        })
+        it ('Book is not affordable.', () => {
+            expect(bookSelection.isItAffordable(15, 10)).to.equal('You don\t have enough money');
+        })
+    });
 
     describe('suitableTitles', function() {
-    })
+    });
 })
