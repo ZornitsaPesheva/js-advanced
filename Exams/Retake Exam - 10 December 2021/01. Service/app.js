92ec3e97eefa3a099277643991c3f27a3a456d33
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2022-06-11 14:21:09 +0300

    so so

--- Exams/Retake Exam - 10 December 2021/01. Service/app.js
+++ Exams/Retake Exam - 10 December 2021/01. Service/app.js
@@ -10,26 +10,57 @@ function solve() {
     sendFormBtn.addEventListener('click', sendForm);
 
     let recievedOrders = document.getElementById('received-orders');
+    let completedOrders = document.getElementById('completed-orders');
+
+    let clear = document.getElementsByClassName('clear-btn')[0];
+    clear.addEventListener('click', clearAll);
     
     function sendForm(e) {
         e.preventDefault();
-        let div = document.createComment('div');
+        let div = document.createElement('div');
         div.classList.add('container');
         recievedOrders.appendChild(div);
         div.appendChild(createElement('h2', 'Product type for иepair: ', productType.value));
         div.appendChild(createElement('h3', 'Client information: ', nameField.value + ', ' + phoneField.value));
         div.appendChild(createElement('h4', 'Description of the problem: ', descritionField.value));
-        div.appendChild(createElement('button', 'Start repair', '', 'start-btn'));
-        div.appendChild(createElement('button', 'Finish repair', '', 'start-btn'));
+        div.appendChild(createElement('button', 'Start repair', '', 'start-btn', start));
+        div.appendChild(createElement('button', 'Finish repair', '', 'finish-btn', finish, true));
     }
 
-    function createElement(el, text, content, elClass) {
+    function createElement(el, text, content, elClass, evListener, disabled) {
         let nodeEl = document.createElement(el);
         nodeEl.textContent = text + content;
         if (elClass) {
             nodeEl.classList.add(elClass);
         }
+        if (evListener) {
+            nodeEl.addEventListener('click', evListener);
+        }
+        if (disabled) {
+            nodeEl.setAttribute('disabled', '');
+        }
         return nodeEl;
+    }
+
+    function start(e) {
+        e.target.setAttribute('disabled', '');
+        e.target.nextSibling.removeAttribute('disabled')
+    }
+
+    function finish(e) {
+        let div = document.createElement(div);
+        div.classList.add('container');
+        div.appendChild(e.target.parentEleemt);
+        completedOrders.appendChild(div);
+        e.target.remove();
+        e.target.parentEleemt.remove();
+    }
+
+    function clearAll() {
+        let allComleted = document.getElementsByClassName(container);
+        for (i = 0; i < allComleted.length; i++) {
+            allComleted[i].remove();
+        }
     }
 
 }
