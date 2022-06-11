b7d4c2bda45ddbbdae66422a65b1504ebb7e37e7
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-11 19:10:19 +0300

    04. Point Distance

--- Classes/Lab/04. Point Distance.js
+++ Classes/Lab/04. Point Distance.js
@@ -3,4 +3,14 @@ class Point {
         this.x = x;
         this.y = y;
     }
+
+    static distance(p1, p2) {
+        return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) **2);
+    }
 }
+
+const point1 = new Point(1, 1);
+const point2 = new Point(4, 5);
+
+
+console.log(Point.distance(point1, point2));
