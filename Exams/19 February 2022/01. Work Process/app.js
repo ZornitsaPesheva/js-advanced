498405ac4c6a306794b373d05bd8667d5889c392
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-11 08:20:38 +0300

    worker added

--- Exams/19 February 2022/01. Work Process/app.js
+++ Exams/19 February 2022/01. Work Process/app.js
@@ -3,12 +3,34 @@ function solve() {
     hireWorkerBtn.addEventListener('click', hireWprkerFunc);
     let inputs = document.querySelectorAll('input');
     [fname, lname, email, birth, position, salary] = inputs;
+    let tableBody = document.querySelector('tbody');
 
     function hireWprkerFunc(e) {
-        e.prebentDefault();
+        e.preventDefault();
         if(![...inputs].some(i => i.value == '')){
+            let tr = document.createElement('tr');
+            tableBody.appendChild(tr);
+            for (i = 0; i < inputs.length; i++) {
+                let td = document.createElement('td');
+                td.textContent = inputs[i].value;
+                console.log(td);
+                tr.appendChild(td);
+                inputs[i].value = '';
+
+            }
+            let td = document.createElement('td');
+            let btn1 = document.createElement('button');
+            btn1.classList.add('fired');
+            btn1.textContent = 'Fired';
+            let btn2 = document.createElement('button');
+            btn2.classList.add('edit');
+            btn2.textContent = 'Edit';
+            td.appendChild(btn1);
+            td.appendChild(btn2);
+            tr.appendChild(td);
             
         } 
+        
     }
 }
 solve()
