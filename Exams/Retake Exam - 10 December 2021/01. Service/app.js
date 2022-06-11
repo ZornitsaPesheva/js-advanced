0ed6eaad4a042ac3c5a4340a93aee23706d64c73
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-11 12:11:39 +0300

    sendForm

--- Exams/Retake Exam - 10 December 2021/01. Service/app.js
+++ Exams/Retake Exam - 10 December 2021/01. Service/app.js
@@ -4,7 +4,7 @@ function solve() {
     let descritionField = document.querySelector('textarea');
     [nameField, phoneField] = document.querySelectorAll('input');
 
-    let type = document.querySelector('select').value;
+    let productType = document.querySelector('select');
 
     let sendFormBtn = document.querySelectorAll('button')[0];
     sendFormBtn.addEventListener('click', sendForm);
@@ -16,7 +16,20 @@ function solve() {
         let div = document.createComment('div');
         div.classList.add('container');
         recievedOrders.appendChild(div);
-        
+        div.appendChild(createElement('h2', 'Product type for иepair: ', productType.value));
+        div.appendChild(createElement('h3', 'Client information: ', nameField.value + ', ' + phoneField.value));
+        div.appendChild(createElement('h4', 'Description of the problem: ', descritionField.value));
+        div.appendChild(createElement('button', 'Start repair', '', 'start-btn'));
+        div.appendChild(createElement('button', 'Finish repair', '', 'start-btn'));
+    }
+
+    function createElement(el, text, content, elClass) {
+        let nodeEl = document.createElement(el);
+        nodeEl.textContent = text + content;
+        if (elClass) {
+            nodeEl.classList.add(elClass);
+        }
+        return nodeEl;
     }
 
 }
