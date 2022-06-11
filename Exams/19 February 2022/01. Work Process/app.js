16af081fa8eb9f22d74d598413137501e7b0f7b8
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-11 08:42:15 +0300

    salary updating

--- Exams/19 February 2022/01. Work Process/app.js
+++ Exams/19 February 2022/01. Work Process/app.js
@@ -7,16 +7,18 @@ function solve() {
 
     function hireWprkerFunc(e) {
         e.preventDefault();
-        if(![...inputs].some(i => i.value == '')){
+        if(![inputs].some(i => i.value == '')){
+            let salary = 0;
             let tr = document.createElement('tr');
             tableBody.appendChild(tr);
             for (i = 0; i < inputs.length; i++) {
                 let td = document.createElement('td');
                 td.textContent = inputs[i].value;
-                console.log(td);
                 tr.appendChild(td);
+                if (inputs[i].id == 'salary'){
+                    salary = inputs[i].value;
+                }
                 inputs[i].value = '';
-
             }
             let td = document.createElement('td');
             let btn1 = document.createElement('button');
@@ -28,6 +30,8 @@ function solve() {
             td.appendChild(btn1);
             td.appendChild(btn2);
             tr.appendChild(td);
+            let budget = document.querySelector('span');
+            budget.textContent = Number(budget.textContent) + Number(salary);
             
         } 
         
