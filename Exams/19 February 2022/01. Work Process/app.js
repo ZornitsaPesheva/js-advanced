27326e63e0b6a65e94dcf4039b725bc7b5a3cf1f
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-11 10:03:59 +0300

    edit

--- Exams/19 February 2022/01. Work Process/app.js
+++ Exams/19 February 2022/01. Work Process/app.js
@@ -27,13 +27,30 @@ function solve() {
             let btn2 = document.createElement('button');
             btn2.classList.add('edit');
             btn2.textContent = 'Edit';
+            btn2.addEventListener('click', editFunc);
             td.appendChild(btn1);
             td.appendChild(btn2);
             tr.appendChild(td);
             let budget = document.querySelector('span');
-            budget.textContent = Number(budget.textContent) + Number(salary);
+            budget.textContent = (Number(budget.textContent) + Number(salary))
+                .toFixed(2);
             
         } 
+
+        function editFunc(e) {
+            e.preventDefault();
+                let row = e.target.parentNode.parentNode;
+                let cells = row.querySelectorAll('td');
+                let j = 0;
+                let salary = 0;
+                for (i = 0; i < cells.length - 1; i++ ){
+                    console.log(inputs[j]);
+                    inputs[j].value = cells[i].textContent;
+                    j++;
+                }
+
+                row.remove();
+        }
         
     }
 }
