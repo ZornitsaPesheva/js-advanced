45cae094855c4e6e6fd27ba26323d0143558c7c2
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-12 18:38:44 +0300

    so so

---
+++ Classes/Exercise/8. Juice Flavors.js
@@ -0,0 +1,43 @@
+function juiceFlavors(array) {
+    
+    let juces = new Map();
+    let bottlesOfJuse = new Map();
+
+    array.forEach(element => {
+        let [juceName, quantity] = element.split(' => ');
+        if (juces.has(juceName)) {
+            let qtt = juces.get(juceName + Number(quantity));
+            juces.set(juceName, qtt);
+            let q = juces.get(juceName) + Number(quantity);
+            juces.set(juceName, q)
+        } else {
+            juces.set(juceName, Number(quantity));
+        }
+
+        if (juces.get(juceName) / 1000 >= 1) {
+            let bottles = Math.floor(juces.get(juceName) / 1000);
+            if (bottlesOfJuse.has(juceName)) {
+                let qb = bottlesOfJuse.get(juceName) + Number(bottles);
+                bottlesOfJuse.set(juceName, qb);
+            } else {
+                bottlesOfJuse.set(juceName, 1);
+            }
+            let juseLeft = juces.get(juceName) % 1000;
+            juces.set(juces.get(juceName), juseLeft);
+        } 
+    });
+
+    for (let [juceName, bottles] of bottlesOfJuse) {
+        console.log(`${juceName} => ${bottles}`);
+    }
+}
+
+juiceFlavors(['Orange => 2000',
+
+'Peach => 1432',
+
+'Banana => 450',
+
+'Peach => 600',
+
+'Strawberry => 549']);
