96ee15f8d450b78fa33675b3bd97e379395cf2be
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-13 07:43:48 +0300

    09. Auto-Engineering Company

--- Classes/Exercise/09. AutoEngineeringCompany.js
+++ Classes/Exercise/09. AutoEngineeringCompany.js
@@ -2,22 +2,48 @@ function autoEngineeringCompany(input) {
     let cars = new Map();
     
     input.forEach(element => {
-        [carBrand, carModel, produceCars] = element.split(' | ');
+        [carBrand, carModel, producedCars] = element.split(' | ');
         if (cars.has(carBrand)) {
             let carModels = cars.get(carBrand);
             if (carModels.has(carModel)) {
-                let qtt = carModels.get(carModel) + Number(produceCars);
+                let qtt = carModels.get(carModel) + Number(producedCars);
                 carModels.set(carModel, qtt);
             }
             else {
-                carModels.set(carModel, produceCars)
+                carModels.set(carModel, Number(producedCars))
             }
         }
         else {
             let newBrand = new Map();
-            newBrand.set(carModel, produceCars);
+            newBrand.set(carModel, Number(producedCars));
             cars.set(carBrand, newBrand);
         }
     });
 
+    for (let [carBrand, carModels] of cars) {
+        console.log(carBrand);
+        for (let [carModel, producedCars] of carModels) {
+            console.log(`###${carModel} -> ${producedCars}`)
+        }
+    }
 }
+
+autoEngineeringCompany(['Audi | Q7 | 1000',
+
+'Audi | Q6 | 100',
+
+'BMW | X5 | 1000',
+
+'BMW | X6 | 100',
+
+'Citroen | C4 | 123',
+
+'Volga | GAZ-24 | 1000000',
+
+'Lada | Niva | 1000000',
+
+'Lada | Jigula | 1000000',
+
+'Citroen | C4 | 22',
+
+'Citroen | C5 | 10']);
