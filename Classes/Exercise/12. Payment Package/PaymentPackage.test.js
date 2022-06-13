571fb67be7aae41c862cb0be6b351665a79b4915
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-13 12:35:35 +0300

    input tested

--- Classes/Exercise/12. Payment Package/PaymentPackage.test.js
+++ Classes/Exercise/12. Payment Package/PaymentPackage.test.js
@@ -3,6 +3,22 @@ const { PaymentPackage } = require('./PaymentPackage')
 
 describe('PaymentPackage', function() {
     it('throw error for missing parameters', () => {
-        expect(new PaymentPackage('HR Services')).to.throw('Value must be a non-negative number');
+        expect(() => new PaymentPackage('HR Services')).to.throw('Value must be a non-negative number');
+    })
+
+    it('throw error for unproper name type', () => {
+        expect(() => new PaymentPackage(1, 1500)).to.throw('Name must be a non-empty string');
+    })
+
+    it('throw error for empty string for name', () => {
+        expect(() => new PaymentPackage('',100)).to.throw('Name must be a non-empty string');
+    })
+
+    it('throw error for unproper type of value', () => {
+        expect(() => new PaymentPackage('HR Services', 'string')).to.throw('Value must be a non-negative number');
+    })
+
+    it('throw error for negative number for value', () => {
+        expect(() => new PaymentPackage('HR Services', -100)).to.throw('Value must be a non-negative number');
     })
 })
