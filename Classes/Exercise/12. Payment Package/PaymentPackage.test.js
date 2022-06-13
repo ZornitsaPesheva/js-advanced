8a45d5aef37c03f57167f0878b16a40bfccf9568
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-13 13:02:08 +0300

    othe malko

--- Classes/Exercise/12. Payment Package/PaymentPackage.test.js
+++ Classes/Exercise/12. Payment Package/PaymentPackage.test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const { PaymentPackage } = require('./PaymentPackage')
 
 describe('PaymentPackage', function() {
+
     it('throw error for missing parameters', () => {
         expect(() => new PaymentPackage('HR Services')).to.throw('Value must be a non-negative number');
     })
@@ -21,4 +22,47 @@ describe('PaymentPackage', function() {
     it('throw error for negative number for value', () => {
         expect(() => new PaymentPackage('HR Services', -100)).to.throw('Value must be a non-negative number');
     })
+
+    describe('getting and setting vakues', function() {
+        it('get name', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(flagClass.name).to.equal('abc');
+        });
+        it('get value', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(flagClass.value).to.equal(123);
+        });
+
+        it('set name', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(flagClass.name = 'def').to.equal(flagClass.name);
+        });
+        it('get value', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(flagClass.valu = 345).to.equal(345);
+        });
+    })
+
+    describe('VAT', function() {
+        it('set VAT should throw errow when the new VAT is a string', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(() => flagClass.VAT = 'abc').to.throw('VAT must be a non-negative number');
+        });
+
+        it('set VAT should throw errow when the new VAT is a negative number', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(() => flagClass.VAT = -20).to.throw('VAT must be a non-negative number');
+        });
+
+        
+    })
+
+    describe('active', function() {
+        it('set active should throw errow when the new value is not boolean', () => {
+            let flagClass = new PaymentPackage('abc', 123);
+            expect(() => flagClass.active = 'abc').to.throw('Active status must be a boolean');
+        });
+    })
+
+
 })
