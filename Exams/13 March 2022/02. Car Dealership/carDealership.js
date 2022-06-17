de14fdf7d1c48a653987bda3d01344aa78c31d5e
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-17 07:11:31 +0300

    sellCar

--- Exams/13 March 2022/02. Car Dealership/carDealership.js
+++ Exams/13 March 2022/02. Car Dealership/carDealership.js
@@ -15,6 +15,23 @@ class CarDealership {
                 throw new Error('Invalid input!');
         }
         this.availableCars.push({model, horsepower, price, mileage});
-        return `New car added: ${model} - ${horsepower} HP - ${mileage} km - ${price}$`
+        return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(2)} km - ${price.toFixed(2)}$`
     }
-}
+
+    sellCar(model, desiredMileage) {
+        let carToSell = this.availableCars.find(c => c.model == model);
+        if (!carToSell) {
+            throw new Error(`${model} was not found!`)
+        }
+        if (carToSell.mileage - desiredMileage > 40000) {
+            carToSell.price -= carToSell.price / 10;
+        }
+        else if (carToSell.mileage - desiredMileage < 40000 && carToSell.mileage - desiredMileage > 0) {
+            carToSell.price -= carToSell.price / 5
+        }
+        this.soldCars.push({model: carToSell.model, horsepower: carToSell.horsepower, soldPrice: carToSell.price});
+        this.availableCars = this.availableCars.filter(c =>  c.model != model);
+        this.totalIncome += this.totalIncome += carToSell.price;
+        return `${model} was sold for ${carToSell.price.toFixed(2)}$`
+    } 
+} 
