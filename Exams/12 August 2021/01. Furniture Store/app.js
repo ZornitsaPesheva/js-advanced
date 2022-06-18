be6c3a50f685cd13c7b502b28f82ca47cd07b628
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-18 19:14:01 +0300

    77

--- Exams/12 August 2021/01. Furniture Store/app.js
+++ Exams/12 August 2021/01. Furniture Store/app.js
@@ -9,27 +9,59 @@ function solve() {
     function addFurniture(e) {
         e.preventDefault();
 
-        let [model, year, price] = document.querySelectorAll('input').value;
+        let inputs = document.querySelectorAll('input');
+        let model = inputs[0].value; 
+        let year = inputs[1].value; 
+        let price = inputs[2].value;
         let description = document.querySelector('textarea');
 
-        if(model != '' && description != '' && year < 0 && price < 0) {
+        if(model != '' && description != '' && year >= 0 && price >= 0) {
             let trInfo = createElement('tr', null, 'info');
             trInfo.appendChild(createElement('td', model));
-            trInfo.appendChild(createElement('td', price));
+            trInfo.appendChild(createElement('td', Number(price).toFixed(2)));
             let tdBtns = createElement('td');
             trInfo.appendChild(tdBtns);
             tdBtns.appendChild(createElement('button', 'More Info', 'moreBtn', more));
             tdBtns.appendChild(createElement('button', 'Buy It', 'buyBtn', buy));
             let trHide = createElement('tr', null, 'hide');
             trHide.appendChild(createElement('td', 'Year: ' + year));
-            let td = document.createComment('td', 'Description: ' + description);
-            td.setAttribute('colspan', 3);
+            let td = document.createElement('td');
+            td.textContent = 'Description: ' + description.value;
+            td.colspan = 3;
             trHide.appendChild(td);
             furnitureList.appendChild(trInfo);
             furnitureList.appendChild(trHide);
+            inputs[0].value = '';
+            inputs[1].value = '';
+            inputs[2].value = '';
+            description.value = '';
+        }
+
+    } 
+
+    function more(e) {
+        let text = e.target.textContent;
+        console.log(text);
+        let hiddenRow = document.getElementsByClassName('hide')[0];
+        if (text == 'More Info') {
+            e.target.textContent = 'Less Info';
+            hiddenRow.style.display = 'contents'; 
+        }
+        else {
+            hiddenRow.style.display = 'none'; 
+            e.target.textContent = 'More Info';
 
         }
-    } 
+    }
+
+    function buy(e) {
+        let tdBtns = e.target.parentElement;
+        console.log(tdBtns);
+        let price = Number(document.getElementsByClassName('total-price')[0].textContent)
+        price += Number(tdBtns.previousSibling.textContent);
+        document.getElementsByClassName('total-price')[0].textContent = price.toFixed(2);
+        tdBtns.parentElement.remove();
+    }
 
     function createElement(type, content, className, evListener) {
         const element = document.createElement(type);
