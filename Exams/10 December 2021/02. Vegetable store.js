962f12bf774699692a595234a996142dd014ca05
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-18 15:30:16 +0300

    100

--- Exams/10 December 2021/02. Vegetable store.js
+++ Exams/10 December 2021/02. Vegetable store.js
@@ -12,8 +12,8 @@ class VegetableStore {
         vegetables.forEach(v => {
             let veg = v.split(' ');
             let type = veg[0];
-            let quantity = veg[1];
-            let price = veg[2];
+            let quantity = Number(veg[1]);
+            let price = Number(veg[2]);
             let vegetable = this.availableProducts.find(v => v.type == type);
             if (vegetable) {
                 vegetable.quantity += quantity;
@@ -29,7 +29,6 @@ class VegetableStore {
                 typesToAdd.push(type);
             }
         });
-
         return 'Successfully added ' + typesToAdd.join(', ');
     }
 
@@ -41,13 +40,12 @@ class VegetableStore {
             let quantity = Number(veg[1]);
             let vegetable = this.availableProducts.find(v => v.type == type)
             if (!vegetable) {
-                throw new Error(`${type} is not available in the store, your current bill is ${totalPrice.toFixed(2)}.`)
+                throw new Error(`${type} is not available in the store, your current bill is $${totalPrice.toFixed(2)}.`)
             }
             if (quantity > vegetable.quantity) {
-                console.log(quantity, vegetable.quantity);
-                throw new Error(`The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is ${totalPrice}.`)
+                throw new Error(`The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is $${totalPrice.toFixed(2)}.`)
             }
-            let priceForType = vegetable.price * quantity;
+            let priceForType = Number(vegetable.price) * quantity;
             totalPrice += priceForType;
             vegetable.quantity -= quantity;
         })
@@ -71,15 +69,18 @@ class VegetableStore {
 
     revision() {
         let result = [];
-        this.availableProducts.forEach(v => result.push(`${v.type}-${v.quantity}-$${v.price}`));
-        result = "Available vegetables:" + result.sort((v1, v2) => v1.price - v2.price).join('\n') 
-            + `The owner of the store is ${this.owner}, and the location is ${this.location}.`
+
+        this.availableProducts
+            .sort((v1, v2) => v1.price - v2.price)
+            .forEach(v => result.push(`${v.type}-${v.quantity}-$${v.price}`));
+        result = "Available vegetables:\n" + result.join('\n') 
+            + `\nThe owner of the store is ${this.owner}, and the location is ${this.location}.`
         return result;
     }
 }
 
- let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
- console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]));
- console.log(vegStore.buyingVegetables(["Okra 1"]));
- console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
- console.log(vegStore.buyingVegetables(["Banana 1", "Beans 2"]));
+let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
+console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]));
+console.log(vegStore.buyingVegetables(["Okra 1"]));
+console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
+console.log(vegStore.buyingVegetables(["Banana 1", "Beans 2"]));
