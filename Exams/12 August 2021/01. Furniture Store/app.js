6d8759f68e26402988a5e9f4b426c7cdbe2d9184
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-18 20:24:08 +0300

    88 pf

--- Exams/12 August 2021/01. Furniture Store/app.js
+++ Exams/12 August 2021/01. Furniture Store/app.js
@@ -5,24 +5,29 @@ function solve() {
     let addBtn = document.getElementById('add');
     addBtn.addEventListener('click', addFurniture);
     const furnitureList = document.getElementById('furniture-list');
+    const totalPriceElement = document.querySelector('.total-price');
 
     function addFurniture(e) {
         e.preventDefault();
 
         let inputs = document.querySelectorAll('input');
         let model = inputs[0].value; 
-        let year = inputs[1].value; 
-        let price = inputs[2].value;
+        let year = Number(inputs[1].value); 
+        let price = Number(inputs[2].value);
         let description = document.querySelector('textarea');
 
-        if(model != '' && description != '' && year >= 0 && price >= 0) {
+        if(
+            model != '' && description != '' && year > 0 && price > 0) {
             let trInfo = createElement('tr', null, 'info');
             trInfo.appendChild(createElement('td', model));
             trInfo.appendChild(createElement('td', Number(price).toFixed(2)));
             let tdBtns = createElement('td');
+
             trInfo.appendChild(tdBtns);
-            tdBtns.appendChild(createElement('button', 'More Info', 'moreBtn', more));
-            tdBtns.appendChild(createElement('button', 'Buy it', 'buyBtn', buy));
+            let moreBtn = createElement('button', 'More Info', 'moreBtn', more);
+            let buyBtn = createElement('button', 'Buy it', 'buyBtn', buy)
+            tdBtns.appendChild(moreBtn);
+            tdBtns.appendChild(buyBtn);
             let trHide = createElement('tr', null, 'hide');
             trHide.appendChild(createElement('td', 'Year: ' + year));
             let td = document.createElement('td');
@@ -35,33 +40,31 @@ function solve() {
             inputs[1].value = '';
             inputs[2].value = '';
             description.value = '';
+
+            function more() {
+                let text = moreBtn.textContent;
+                
+                if (text == 'More Info') {
+                    moreBtn.textContent = 'Less Info';
+                    trHide.style.display = 'contents'; 
+                }
+                else {
+                    trHide.style.display = 'none'; 
+                    moreBtn.textContent = 'More Info';
+        
+                }
+            }
+
+            function buy() {
+                let totalPrice = Number(totalPriceElement.textContent)
+                totalPrice += price;
+                totalPriceElement.textContent = totalPrice.toFixed(2);
+                trInfo.remove();
+                trHide.remove();
+            }
         }
 
     } 
-
-    function more(e) {
-        let text = e.target.textContent;
-        console.log(text);
-        let hiddenRow = document.getElementsByClassName('hide')[0];
-        if (text == 'More Info') {
-            e.target.textContent = 'Less Info';
-            hiddenRow.style.display = 'contents'; 
-        }
-        else {
-            hiddenRow.style.display = 'none'; 
-            e.target.textContent = 'More Info';
-
-        }
-    }
-
-    function buy(e) {
-        let tdBtns = e.target.parentElement;
-        console.log(tdBtns);
-        let price = Number(document.getElementsByClassName('total-price')[0].textContent)
-        price += Number(tdBtns.previousSibling.textContent);
-        document.getElementsByClassName('total-price')[0].textContent = price.toFixed(2);
-        tdBtns.parentElement.remove();
-    }
 
     function createElement(type, content, className, evListener) {
         const element = document.createElement(type);
