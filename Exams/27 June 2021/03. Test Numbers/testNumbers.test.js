23244bca438032c38044c8f0ae77f1fffb666605
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-20 09:11:09 +0300

    90

--- Exams/27 June 2021/03. Test Numbers/testNumbers.test.js
+++ Exams/27 June 2021/03. Test Numbers/testNumbers.test.js
@@ -6,5 +6,29 @@ describe('testNumbers', () => {
         it('returns undefined if parameters are not numbers', () => {
             expect(testNumbers.sumNumbers('4', 5)).to.equal(undefined);
         })
+        it('returns undefined if parameters are not numbers', () => {
+            expect(testNumbers.sumNumbers(5, 'text')).to.equal(undefined);
+        })
+        it('returns correct sum', () => {
+            expect(testNumbers.sumNumbers(5, 3)).to.equal('8.00');
+        })
+    })
+
+    describe('numberChecker', () => {
+        it('returns not a number', () => {
+            expect(() => testNumbers.numberChecker('text').to.trow('The input is not a number!'))
+        })
+        it ('returns corret even', () => {
+            expect(testNumbers.numberChecker(4)).to.equal('The number is even!')
+        })
+        it ('returns corret odd', () => {
+            expect(testNumbers.numberChecker(5)).to.equal('The number is odd!')
+        })
+    })
+
+    describe('averageSumArray', () => {
+        it ('returns correct avarage', () => {
+            expect(testNumbers.averageSumArray([2, 4, 6])).to.equal(4);
+        })
     })
 })
