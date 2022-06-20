702d981d113e4d8930778693b9ebbd4ec08273a6
balkangraph <zornitsa.pesheva@outlook.com>
2022-06-20 05:37:27 +0300

    loadProducts

--- Exams/27 June 2021/02. Restaurant.js
+++ Exams/27 June 2021/02. Restaurant.js
@@ -11,8 +11,21 @@ class Restaurant {
     loadProducts(array) {
         array.forEach(p => {
             let [productName, productQuantity, productTotalPrice] = p.split(' ');
-            
+            if (productTotalPrice <= this.budgetMoney) {
+                if (productName in this.stockProducts){
+                    this.stockProducts[productName] =+ productQuantity;
+                }
+                else {
+                    this.stockProducts[productName] = productQuantity;
+                }
+                this.budget -= productTotalPrice; 
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`)
+            }
+            else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`)
+            }
         });
+        return this.history.join('\n');
     }
 
 }
