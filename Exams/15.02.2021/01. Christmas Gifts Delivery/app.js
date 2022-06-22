abcda103bb2419ea8e6fda64797ca7449ba219b3
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-06-22 14:44:31 +0300

    80

--- Exams/15.02.2021/01. Christmas Gifts Delivery/app.js
+++ Exams/15.02.2021/01. Christmas Gifts Delivery/app.js
@@ -4,11 +4,36 @@ function solution() {
     addBtn.addEventListener('click', addGift);
     let name = document.querySelector('input');
     let [listOfGifts, sentGifts, discardedGifts] = document.querySelectorAll('ul');
+    sortedList = [];
 
     function addGift(e) {
-        let gift = document.createElement('li');
-        gift.textContent = name.value;
-        listOfGifts.appendChild(gift);
-        
+        listOfGifts.innerHTML = '';
+        sortedList.push(name.value);
+        sortedList.sort((a, b) => a.localeCompare(b));
+        for (let g of sortedList) {
+            let gift = document.createElement('li');
+            gift.textContent = g;
+            gift.classList.add('gift');
+            listOfGifts.appendChild(gift);
+            let sendBtn = document.createElement('button');
+            sendBtn.id = 'sendButton';
+            sendBtn.textContent = 'Send';
+            let discardBtn = document.createElement('button');
+            discardBtn.id = 'discardButton';
+            discardBtn.textContent = 'Discard';
+            gift.appendChild(sendBtn);
+            gift.appendChild(discardBtn);
+            sendBtn.addEventListener('click', function() {
+                sentGifts.appendChild(gift);
+                discardBtn.remove();
+                sendBtn.remove();
+            });
+            discardBtn.addEventListener('click', function() {
+                discardedGifts.appendChild(gift);
+                sendBtn.remove();
+                discardBtn.remove();
+            })
+        }
+        name.value = '';
     }
 }
