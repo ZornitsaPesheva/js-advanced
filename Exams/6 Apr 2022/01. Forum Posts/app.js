1cccc7a397ed3d94a6fd86893c339b8f9c586ace
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-06-23 14:50:16 +0300

    neznam

--- Exams/6 Apr 2022/01. Forum Posts/app.js
+++ Exams/6 Apr 2022/01. Forum Posts/app.js
@@ -5,10 +5,12 @@ function solve() {
   publishBtn.addEventListener('click', publish);
 
   const reviewList = document.getElementById('review-list');
+  const publishedList = document.getElementById('published-list');
   const [title, category] = document.getElementsByTagName('input');
   const text = document.getElementById('post-content');
 
-  function publish() {
+  function publish(e) {
+    e.preventDefault();
     if (title.value != '' && category.value != '' && text.value != ''){
       let postTitle = title.value;
       let postCategory = category.value;
@@ -19,10 +21,39 @@ function solve() {
       let article = document.createElement('article');
       li.appendChild(article);
       let h4 = createElement('h4');
-      h4.textContent = title.value;
+      h4.textContent = postTitle;
       article.appendChild(h4);
       let p1 = document.createElement('p');
-      p1.textContent = 
+      p1.textContent = 'Category:' + postCategory;
+      article.appendChild(p1);
+      let p2 = document.createElement('p');
+      p2.textContent = 'Content: ' + postText;
+      article.appendChild(p2);
+      let editBtn = document.createElement('button');
+      editBtn.classList.add('action-btn');
+      editBtn.classList.add('edit');
+      editBtn.textContent = 'Edit';
+      li.appendChild(editBtn);
+      let approveBtn = document.createElement('button');
+      approveBtn.classList.add('action-btn');
+      approveBtn.classList.add('approve');
+      approveBtn.textContent = 'Approve';
+      li.appendChild(approveBtn);
+      title.value = '';
+      category.value = '';
+      text.value = '';
+      editBtn.addEventListener('click', function() {
+        title.value = postTitle;
+        category.value = postCategory;
+        text.value = postText;
+        li.remove();
+      });
+      approveBtn.addEventListener('click', function() {
+        publishedList.appendChild(li);
+        editBtn.remove();
+        approveBtn.remove();
+      })
+
     }
   }
 
