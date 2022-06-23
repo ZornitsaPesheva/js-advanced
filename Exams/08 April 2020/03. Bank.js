3e187b3332698748b0e44b68323ba5abfaaed558
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-06-23 08:20:06 +0300

    depositMoney

--- Exams/08 April 2020/03. Bank.js
+++ Exams/08 April 2020/03. Bank.js
@@ -16,5 +16,15 @@ class Bank {
         return {firstName, lastName, personalId};
     }
 
-    
+    depositMoney(personalId, amount) {
+        let customer = this.allCustomers.find(c => c.personalId == personalId);
+        if (!customer){
+            throw new Error('We have no customer with this ID!');
+        }
+        if (!customer[totalMoney]) {
+            customer[totalMoney] = 0
+        }
+        customer[totalMoney] += amount;
+        return `${customer[totalMoney]}$`
+    }
 }
